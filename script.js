7697f13be6ee4c277dfa2ac8914a2407a1ad34a9
Sneha M S <70563772+sehanms@users.noreply.github.com>
2020-12-23 23:19:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+  const daysEl = document.getElementById('days');
+  const hoursEl = document.getElementById('hours');
+  const minsEl = document.getElementById('mins');
+  const  secsEl = document.getElementById('secs');
+
+  const newYears="1 Jan 2021";
+
+  function countdown(){
+      const newYearsDate = new Date(newYears);
+      const currentDate = new Date();
+
+      const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+      const days = Math.floor(totalSeconds / 3600 /24);
+      const hours = Math.floor(totalSeconds / 3600) % 24;
+      const mins = Math.floor(totalSeconds / 60) % 60;
+      const secs = Math.floor(totalSeconds) % 60;
+
+      daysEl.innerHTML = days;
+      hoursEl.innerHTML = hours;
+      minsEl.innerHTML = mins;
+      secsEl.innerHTML = secs;
+  }
+  //initial call
+  countdown();
+
+  setInterval(countdown,1000);
